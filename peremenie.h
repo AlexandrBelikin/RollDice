8dabfa094f6a6dbe391e6f7f286cb6956f5f7c93
AlexandrBelikin <52128925+AlexandrBelikin@users.noreply.github.com>
2019-06-25 15:19:24 +0300

    Add files via upload

---
+++ peremenie.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "stdafx.h"
+#include <array>
+#include <string>
+using namespace std;
+int dicecount, sumdice=0;
+int kubik,kol_vo_kubov,puliAMOUNT,pulaDMG;
+int ZonaVremenno;
+int *Dice = new int[kubik];
+bool critsuccess=false,critfail=false,normal;
+int random,counter;
+int vibor,uspehi;
+int modificator,protiv;
+char input;
+int damage;
+string PlayerName;
